b565c5a17df89a19f64aa02d21572e59ab04a70c
Hadi Haidar <hadi12121211@gmail.com>
2025-08-25 18:24:30 +0300

    update session.php

--- config/session.php
+++ config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'localhost'),
+    'domain' => env('SESSION_DOMAIN', null), // Change from 'localhost' to null
 
     /*
     |--------------------------------------------------------------------------
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', true), // Enable for HTTPS
 
     /*
     |--------------------------------------------------------------------------
