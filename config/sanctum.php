691c8b1c7b6a1966152122b6fb9b8686a17e7139
Hadi Haidar <hadi12121211@gmail.com>
2025-09-12 10:42:24 +0300

    disable csrf token from backend

--- config/sanctum.php
+++ config/sanctum.php
@@ -75,7 +75,8 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        // Remove the CSRF line below for API-only usage
+        // 'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
